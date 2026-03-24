4aa2ec0055bd8976beb3671e53728413c1c5aadc
ISTEC-FOR340 <jorge.forero@my.istec.pt>
2026-03-24 10:36:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     const routes = {
-        "btn-p1": "proyecto2/proyecto2.html",
-        "btn-p2": "proyecto1/proyecto1.html"
+        "btn-p1": "proyecto1/proyecto1.html",
+        "btn-p2": "proyecto2/proyecto2.html"
     };
 
     Object.keys(routes).forEach(id => {
